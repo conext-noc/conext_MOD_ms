12c58c48a686f57ea59cf0d68b1334c29f210577
Ricardo Vera <ricardovera71@gmail.com>
2023-07-24 21:52:12 -0500

    fix(cors)

--- mod/urls.py
+++ mod/urls.py
@@ -3,5 +3,5 @@ from .views import MOD, MODDashboard
 
 urlpatterns = [
     path("", MOD.as_view()),
-    path("mod-dashboard", MODDashboard.as_view()),
+    path("dashboard", MODDashboard.as_view()),
 ]
--- ms_mod/settings.py
+++ ms_mod/settings.py
@@ -47,6 +47,23 @@ CORS_ALLOWED_ORIGINS = [
 ]
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+]
+
+CORS_ALLOW_HEADERS = [
+    "Accept",
+    "Accept-Language",
+    "Content-Language",
+    "Content-Type",
+]
 
 # Application definition
 
@@ -70,6 +87,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "ms_mod.urls"
